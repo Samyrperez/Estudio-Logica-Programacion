5691f7b9f88db19f8410b581480c03bb90b15581
Samyrperez <samyr.perez.ttc.485@unilibre.edu.co>
2025-06-02 12:42:04 -0500

    Inicio de estudio de logica

---
+++ modulo-1/fundamentos-logica-programacion.js
@@ -0,0 +1,16 @@
+/*
+🟢 MÓDULO 1: Fundamentos de la Lógica de Programación
+🎯 Objetivo:
+Comprender los conceptos básicos de la lógica de programación, necesarios para construir cualquier algoritmo o programa.
+
+
+📌 1. ¿Qué es la lógica de programación?
+Es la capacidad de pensar paso a paso para resolver problemas usando instrucciones que una computadora pueda entender. Es como darle órdenes a una máquina, pero sin ambigüedad.
+
+Ejemplo de lógica básica en la vida real:
+
+🧠 "Si tengo hambre, abro la heladera. Si hay comida, como. Si no hay, pido delivery."
+
+Eso es un algoritmo lógico.
+
+*/
